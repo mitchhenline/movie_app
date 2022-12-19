0905d66beaded31284e932570927657252ed1e23
Mitch Henline <rmhenline@gmail.com>
2022-12-19 12:19:13 -0700

    first commit

---
+++ model.py
@@ -0,0 +1,59 @@
+"""Models for movie ratings app."""
+import os
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+# /////////////////////USER CLASS/////////////////////////
+
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement = True, primary_key = True)
+    email = db.Column(db.String, unique = True)
+    password = db.Column(db.String)
+
+    def __repr__(self):
+            return f'<User user_id={self.user_id} email={self.email}>'
+
+# /////////////////////MOVIE CLASS/////////////////////////  
+
+class Movie(db.Model):
+
+    __tablename__ = "movies"
+
+    movie_id = db.Column(db.Integer, autoincrement = True, primary_key = True)
+    title = db.Column(db.String)
+    overview = db.Column(db.Text)
+    release_date = db.Column(db.DateTime)
+    poster_path = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Movie movie_id: {self.movie} title: {self.title} >'
+
+
+
+
+def connect_to_db(flask_app, db_uri=os.environ["POSTGRES_URI"], echo=False):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+
+if __name__ == "__main__":
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
