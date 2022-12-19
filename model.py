cde735dd66a989f4eec826b894c1f9346cc3ef71
Mitch Henline <rmhenline@gmail.com>
2022-12-19 13:00:32 -0700

    data modeling and setup complete

--- model.py
+++ model.py
@@ -17,6 +17,8 @@ class User(db.Model):
     email = db.Column(db.String, unique = True)
     password = db.Column(db.String)
 
+    # ratings = a list of rating objects
+
     def __repr__(self):
             return f'<User user_id={self.user_id} email={self.email}>'
 
@@ -32,8 +34,28 @@ class Movie(db.Model):
     release_date = db.Column(db.DateTime)
     poster_path = db.Column(db.String)
 
+    # ratings = a list of rating objects
+
     def __repr__(self):
-        return f'<Movie movie_id: {self.movie} title: {self.title} >'
+        return f'<Movie movie_id: {self.movie_id} title: {self.title} >'
+
+# /////////////////////RATING CLASS/////////////////////////  
+
+class Rating(db.Model):
+
+    __tablename__ = "ratings"
+
+    rating_id = db.Column(db.Integer, autoincrement = True, primary_key = True)
+    score = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer, db.ForeignKey("movies.movie_id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+
+    movie = db.relationship("Movie", backref ="ratings")
+    user = db.relationship("User", backref ="ratings")
+
+
+    def __repr__(self):
+        return f'<Rating rating_id: {self.rating_id} score: {self.score} >'
 
 
 
