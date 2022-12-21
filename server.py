f91ad82c49a144970095ddb8592de68368b0ee05
Mitch Henline <rmhenline@gmail.com>
2022-12-21 11:22:47 -0700

    starting work on server

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 """Server for movie ratings app."""
-
-from flask import Flask
+from flask import Flask, render_template, request, flash, session, redirect
+from model import connect_to_db, db
+import crud
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
 
